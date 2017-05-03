74a197cb38cb8d28f44b4d7e88a7689b5c7cef9d
Nathaniel Meyer and Riley Watts <nathaniel@>
2017-05-03 10:52:18 -0700

    Initial Angular structure

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Welcome to the taproom</h1>
+  `
+})
+
+export class AppComponent {
+
+}
